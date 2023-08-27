94368c20993627a1a93fb56940419814114fa009
Alex <35335834+Evlampyev@users.noreply.github.com>
2023-08-27 17:18:59 +0500

    HomeWork -1 is ready

---
+++ HomeWorks/Task_9_JuliaRyzhova/ReadMe.md
@@ -0,0 +1,11 @@
+__Юлии Рыжовой программа.__
+
+[requirements.txt — что это и зачем?](https://semakin.dev/2020/04/requirements_txt/)  
+
+_pip install -r requirements.txt_ - команда для установки всех пакетов, 
+которые указаны в __requirements.txt__
+
+[Ссылка](https://github.com/JuliaRyzhova/Software_architecture.git) на её репозиторий на Git
+
+
+[Скачивание только одного каталога с Git](https://download-directory.github.io/)
---
+++ HomeWorks/Task_9_JuliaRyzhova/config.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+
+# конфигурация
+DATABASE = '/tmp/books_data.db'
+DEBUG = True
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Загружаем конфиг по умолчанию и переопределяем в конфигурации часть
+# значений через переменную окружения
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'books_data.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
